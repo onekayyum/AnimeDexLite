ce08dd2f4c0c466c8655cdac401ebae161deffef
kayyum khan <152464768+onekayyum@users.noreply.github.com>
2025-06-10 12:20:17 +0530

    Update episode.js

--- js/episode.js
+++ js/episode.js
@@ -1,6 +1,6 @@
 // Api urls
 
-const ProxyApi = "https://proxy.techzbots1.workers.dev/?u=";
+const ProxyApi = "https://wispy-truth-ac6b.kayyumkhan342001.workers.dev/?u=";
 const animeapi = "/anime/";
 const episodeapi = "/episode/";
 const dlapi = "/download/";
