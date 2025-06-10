d6bd5f0f6d0591ebe6405685b5dc571bf57de469
kayyum khan <152464768+onekayyum@users.noreply.github.com>
2025-06-10 12:08:08 +0530

    Update anime.js

--- js/anime.js
+++ js/anime.js
@@ -1,6 +1,6 @@
 // Api urls
 
-const ProxyApi = "https://proxy.techzbots1.workers.dev/?u="
+const ProxyApi = "https://api3.kayyumkhan342001.workers.dev/?u="
 const animeapi = "/anime/";
 const recommendationsapi = "/recommendations/";
 
